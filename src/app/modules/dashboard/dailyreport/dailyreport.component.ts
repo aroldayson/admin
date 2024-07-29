5fc75246f2d132fac72d89649bd03b8c4ef750ab
Arold Ayson <aroldayson3@gmail.com>
2024-07-29 06:00:07 -0700

    update

---
+++ src/app/modules/dashboard/dailyreport/dailyreport.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dailyreport',
+  standalone: true,
+  imports: [],
+  templateUrl: './dailyreport.component.html',
+  styleUrl: './dailyreport.component.css'
+})
+export class DailyreportComponent {
+
+}
