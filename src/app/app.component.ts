10ad029ea45bca2862a6b0bb166dc2a0e8feea7e
Arold Ayson <aroldayson3@gmail.com>
2024-07-30 07:07:43 -0700

    updated sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'admin';
+  
+
+  ngOnInit(): void {
+    // initFlowbite();
+  }
+  isOpen = false;
+  open = false;
+  openNav() {
+    this.isOpen = true;
+  }
+
+  closeNav() {
+    this.isOpen = false;
+  }
+
+  toggleNav() {
+    this.isOpen = !this.isOpen;
+  }
 }
